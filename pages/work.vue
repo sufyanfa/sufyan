d3dcb31cc74b4097effea95e904b999a117d092e
Sufyan Farea <33679576+sufyanfa@users.noreply.github.com>
2020-12-12 00:36:03 +0300

    Update work.vue

--- pages/work.vue
+++ pages/work.vue
@@ -1,149 +1,58 @@
 <template>
-  <section class="bg-white dark:bg-gray-900 min-h-screen md:flex flex-col items-center justify-center relative">
-        <div class="pt-24 md:pt-0 pb-10 md:pb-10">
-            <h2 class="text-4xl font-bold text-center text-gray-800 dark:text-gray-100"> {{$t('work')}}</h2>
-        </div>
-        <ul class="flex items-center justify-around md:space-x-4 md:space-x-reverse px-6"> 
-            <li class="flex flex-col items-center" v-for="item in shoppingItems" :key="item.id">
-                <div class="bg-gradient-to-tr from-indigo-500 to-blue-600 rounded-full p-1">
-                    <nuxt-link :to="'#' + item.id" class="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300">
-                        <img @click="showModal()" class="h-12 md:h-24 w-12 md:w-24 rounded-full" src="/images/bg-project.jpg" alt="image">
-                    </nuxt-link>
-                </div>
-                <p class="text-gray-900 dark:text-gray-100 mt-4 text-sm md:text-md">{{ item.name }}</p>
-            </li>
-        </ul>
-      <Skills />
-      <modal v-show="show_modal" class="h-full w-full absolute top-52">
-          <div class="modal-wrapper flex items-center">
-              <div class="modal max-w-md mx-auto xl:max-w-5xl lg:max-w-5xl md:max-w-2xl bg-white max-h-screen shadow-lg flex-row rounded relative">
-                <div class="modal-body p-5 w-full h-full overflow-y-auto ">
-                  <div class="flex flex-col justify-center items-center max-w-lg mx-auto">
-                    <div class="bg-gray-300 h-52 w-full rounded-lg shadow-md bg-cover bg-center" style="background-image: url(/images/bg-project.jpg)"></div>
-                      <div class="w-56 md:w-64 bg-white rounded-lg overflow-hidden">
-                        <h3 class="mx-auto text-center py-2 text-indigo-500">g</h3>
-                        <p class="mx-auto text-center text-gray-800">ewdwed</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="modal-footer py-3 px-5 border0-t text-right">
-                    <button class="bg-indigo-500 px-5 py-2 text-white rounded" @click="showModal()">OK</button>
-                  </div>
-              </div>
+  <section class="min-h-screen md:flex flex-col items-center justify-center p-4 md:p-0">
+    <div class="container mx-auto pt-16">
+      <div class="mb-10">
+        <h1 class="xl:text-5xl pt-4 xl:pt-0 text-3xl text-greenone text-center font-extrabold mb-4">أعمالي</h1>
+        <p class="text-xl text-gray-600 dark:text-gray-300 pt-4 text-center xl:w-3/5 mx-auto w-11/12">
+            معرض الأعمال
+        </p>
+      </div>
+      <div class="lg:flex md:flex xl:justify-around sm:flex flex-wrap md:justify-around sm:justify-around lg:justify-around">
+        <div class="xl:w-1/3 sm:w-5/12 sm:max-w-xs relative mb-20 lg:mb-20 xl:max-w-sm lg:w-1/2 w-11/12 mx-auto sm:mx-0">
+          <div class="shadow h-64 rounded z-10">
+            <img src="/images/project/brand.png" class="h-full w-full object-cover border border-gray-300 overflow-hidden rounded" />
           </div>
-    </modal>
+          <div class="p-6 shadow-lg w-11/12 mx-auto -mt-20 bg-white dark:bg-gray-800 rounded z-20 relative">
+            <nuxt-link to="/">
+              <p class="uppercase text-lg text-greenone text-center pb-3">براند</p>
+              <p class="text-md text-gray-800 dark:text-gray-100 text-center pb-3">موقع شركة رقمية متخصصة في التسويق الإلكتروني</p>
+            </nuxt-link>
+          </div>
+        </div>
+        <div class="xl:w-1/3 sm:w-5/12 sm:max-w-xs relative mb-20 lg:mb-20 xl:max-w-sm lg:w-1/2 w-11/12 mx-auto sm:mx-0">
+          <div class="shadow h-64 rounded z-10">
+            <img src="/images/project/araam.png" class="h-full w-full object-cover border border-gray-300 overflow-hidden rounded" />
+          </div>
+          <div class="p-6 shadow-lg w-11/12 mx-auto -mt-20 bg-white dark:bg-gray-800 rounded z-20 relative">
+            <nuxt-link to="/">
+              <p class="uppercase text-lg text-greenone text-center pb-3">آرام</p>
+              <p class="text-md text-gray-800 dark:text-gray-100 text-center pb-3">موقع لطلاب المرحلة الثانوية </p>
+            </nuxt-link>
+          </div>
+        </div>
+        <div class="xl:w-1/3 sm:w-5/12 sm:max-w-xs relative mb-20 lg:mb-20 xl:max-w-sm lg:w-1/2 w-11/12 mx-auto sm:mx-0">
+          <div class="shadow h-64 rounded z-10">
+            <img src="/images/project/me.png" class="h-full w-full object-cover border border-gray-300 overflow-hidden rounded" />
+          </div>
+          <div class="p-6 shadow-lg w-11/12 mx-auto -mt-20 bg-white dark:bg-gray-800 rounded z-20 relative">
+            <nuxt-link to="/">
+              <p class="uppercase text-lg text-greenone text-center pb-3">موقعي</p>
+              <p class="text-md text-gray-800 dark:text-gray-100 text-center pb-3">موقعي الشخصي</p>
+            </nuxt-link>
+          </div>
+        </div>
+        <div class="xl:w-1/3 sm:w-5/12 sm:max-w-xs relative mb-20 lg:mb-20 xl:max-w-sm lg:w-1/2 w-11/12 mx-auto sm:mx-0">
+          <div class="shadow h-64 rounded z-10">
+            <img src="/images/project/project.jfif" class="h-full w-full object-cover border border-gray-300 overflow-hidden rounded" />
+          </div>
+          <div class="p-6 shadow-lg w-11/12 mx-auto -mt-20 bg-white dark:bg-gray-800 rounded z-20 relative">
+            <nuxt-link to="/">
+              <p class="uppercase text-lg text-greenone text-center pb-3">دليل البرمجة</p>
+              <p class="text-md text-gray-800 dark:text-gray-100 text-center pb-3">إضافة لتعلم البرمجة للمبتدئين بطريقة مهام</p>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
-
-<script>
-import Skills from '../components/Skills.vue'
-export default {
-    components: { Skills },
-    data() {
-      return {
-        title: 'Home page',
-        show_modal: false,
-        shoppingItems : [
-          {
-            "id": 1,
-            "name": 'آرام',
-            "title": "72.186.179.245",
-            "image": 'http://dummyimage.com/154x153.png/ff4444/ffffff'
-          }
-          ,
-          {
-            "id": 2,
-            "name": "إحسان",
-            "title": "25.64.108.109",
-            "image": "http://dummyimage.com/154x153.png/ff4444/ffffff"
-          }, {
-            "id": 3,
-            "name": "سفيان",
-            "title": "200.65.145.138",
-            "image": "http://dummyimage.com/138x106.png/5fa2dd/ffffff"
-          }, {
-            "id": 4,
-            "name": "براند",
-            "title": "75.40.238.61",
-            "image": "http://dummyimage.com/148x130.jpg/5fa2dd/ffffff"
-          }, {
-            "id": 5,
-            "name": "شيكرز",
-            "title": "5.153.166.127",
-            "image": "http://dummyimage.com/233x229.png/ff4444/ffffff"
-          }
-        ]
-      }
-    },
-    methods:{
-        showModal(){
-            if(this.show_modal){
-            //stop screen scrolling
-              document.getElementsByTagName("html")[0].classList.remove('overflow-y-hidden'); 
-              this.show_modal = false;
-            }else{
-              document.getElementsByTagName("html")[0].classList.add('overflow-y-hidden');
-              this.show_modal = true;
-            }
-        }
-    },
-    head() {
-      return {
-        title: this.title,
-        meta: [
-          {
-            hid: 'description',
-            name: 'description',
-            content: 'Home page description'
-          }
-        ]
-      }
-    }
-}
-</script>
-
-<style>
-.modal-body{
-    max-height: 500px;
-  }
-  .bg-gray-800-opacity{
-    background-color: #2D374850;
-  }
-  @media screen and (max-width: 768px){
-    .modal-body {
-        max-height: 400px;
-    }
-  }
-
-  /* animation for vue transition tag */
-
-  .fade-up-down-enter-active {
-    transition: all 0.3s ease;
-  }
-  .fade-up-down-leave-active {
-    transition: all 0.3s ease;
-  }
-  .fade-up-down-enter {
-    transform: translateY(10%);
-    opacity: 0;
-  }
-  .fade-up-down-leave-to {
-    transform: translateY(10%);
-    opacity: 0;
-  }
-
-  .fade-enter-active{
-      -webkit-transition: opacity 2s;
-      transition: opacity .3s;
-
-  }
-  .fade-leave-active {
-      transition: opacity .3s;
-  }
-
-  .fade-enter,
-  .fade-leave-to{
-      opacity: 0;
-  }
-  
-</style>
